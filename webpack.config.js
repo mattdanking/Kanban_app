6c58d75303832c8500c06ccffb449c53b0ec9f6a
Matt King <mattdanking@gmail.com>
2016-05-05 17:12:45 -0400

    split webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+// module.exports = {
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
+const common = {
+
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
@@ -16,3 +24,14 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+
+// Default configuration. We will return this if
+// Webpack is called outside of npm.
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
